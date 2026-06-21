cacfe12d66250e9bc99a7141894144ac094b7c6b
bahouadonald <bahouadonald@gmail.com>
2026-06-21 15:35:59 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -14,4 +14,5 @@ if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js').catch(() => {})
   })
+if (window.__hideSplash) window.__hideSplash();
 }
